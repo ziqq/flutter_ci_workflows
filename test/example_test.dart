4f41f64cbc3aaeaf629531c5145af696c3c0effa
AntØn UstinØff <a.a.ustinoff@gmail.com>
2025-11-06 16:27:43 +0400

    chore: update example

---
+++ test/example_test.dart
@@ -0,0 +1,9 @@
+/*
+ * Author: Anton Ustinoff <https://github.com/ziqq> | <a.a.ustinoff@gmail.com>
+ * Date: 06 November 2025
+ */
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() => group('Unit_tests -', () => debugPrint('Unit tests passed!'));
